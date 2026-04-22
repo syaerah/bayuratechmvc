6ba4df5df4167dd4ddf0ead7fc01596dae53c269
caerah99 <syaerahzolkipli@gmail.com>
2026-04-23 01:20:11 +0800

    Belajar method & params

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,6 +1,22 @@
 <?php
     class Pages {
         public function __construct(){
-            echo 'Pages loaded';
+            //echo 'Pages loaded';
+        }
+
+        public function index($id){
+            echo 'index loaded';
+        }
+
+        /* ni contoh 1
+        public function about(){
+            echo 'Ni page about';
+        }
+        */
+
+        //ni contoh 2
+        public function about($id){
+            echo $id;
         }
     }
+
--- app/libraries/core.php
+++ app/libraries/core.php
@@ -18,7 +18,7 @@
 
             //sambungan
             $url = $this->getUrl();
-
+            
             //nak tengok controller untuk 1st value
             if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
                 //kalau exists, set as controller
@@ -32,6 +32,25 @@
 
             //Instantiate controller class
             $this->currentController = new $this->currentController;
+
+            // okay yang ni url 2nd value
+            if(isset($url[1])){
+                //nak check method to ada tak dalam controller
+                if(method_exists($this->currentController, $url[1])){
+                    $this->currentMethod = $url[1];
+                    //unset 1 index
+                    unset($url[1]);
+
+                }
+            } 
+
+            //echo $this->currentMethod; //yang ni part kalau nak display kat http://localhost/bayuratechmvc/pages/about perkataan about
+
+            //Get params
+            $this->params = $url ? array_values($url) : [];
+
+            //panggil balik array of params
+            call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
         }
 
         public function getUrl(){
