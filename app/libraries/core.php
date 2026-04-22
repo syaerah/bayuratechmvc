a74d0a462d5e40a81d5da3a01b7b5008a712d9f7
caerah99 <syaerahzolkipli@gmail.com>
2026-04-22 21:00:53 +0800

    Belajar controller

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -11,10 +11,39 @@
         protected $currentParams = [];
 
         public function __construct() {
-            $this->getUrl();
+            // echo $_GET['url']; (yang ni untuk bab bootstrap & core)
+
+            // ni untuk belajar loading controller
+            // print_r($this->getUrl());
+
+            //sambungan
+            $url = $this->getUrl();
+
+            //nak tengok controller untuk 1st value
+            if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+                //kalau exists, set as controller
+                $this->currentController = ucwords($url[0]);
+                //unset 0 Index
+                unset($url[0]);
+            }
+
+            //require controller
+            require_once '../app/controllers/' . $this->currentController . '.php';
+
+            //Instantiate controller class
+            $this->currentController = new $this->currentController;
         }
 
         public function getUrl(){
-            echo $_GET['url'];
+
+            if(isset($_GET['url'])){
+                $url = rtrim ($_GET['url'], '/');
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                $url = explode('/', $url);
+                return $url;
+            }
         }
     }
+
+
+        
