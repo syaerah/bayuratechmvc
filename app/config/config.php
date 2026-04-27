31f5085fb31bd40f556ff68db34990a825e4f852
caerah99 <syaerahzolkipli@gmail.com>
2026-04-27 21:02:04 +0800

    debug

--- app/config/config.php
+++ app/config/config.php
@@ -19,7 +19,7 @@
     define ('APPROOT', dirname(dirname(__FILE__)));
 
     //URL root
-    define ('URLROOT', 'http://localhost/bayuratechmvc');
+    define ('URLROOT', 'http://localhost/bayuratechmvc/public');
 
     //Site names
     define('SITENAME', 'BayuratechMVC');
--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,1 +1,7 @@
-ABOUTTT
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+
+    <h1><?php echo $data['title']; ?></h1>
+
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
